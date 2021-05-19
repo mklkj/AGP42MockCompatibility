8cb60a04d134b5af206f2b2aaf8f60284dcbb0d3
Mikołaj Pich <m.pich@outlook.com>
2021-05-19 23:09:09 +0200

    Add flow utils with tests

---
+++ app/src/main/java/io/github/mklkj/agp_mock_compatibility/Resource.kt
@@ -0,0 +1,24 @@
+package io.github.mklkj.agp_mock_compatibility
+
+
+data class Resource<T>(val status: Status, val data: T?, val error: Throwable?) {
+    companion object {
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(error: Throwable?, data: T? = null): Resource<T> {
+            return Resource(Status.ERROR, data, error)
+        }
+
+        fun <T> loading(data: T? = null): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+    }
+}
+
+enum class Status {
+    LOADING,
+    SUCCESS,
+    ERROR
+}
